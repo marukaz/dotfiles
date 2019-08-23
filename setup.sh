a3d6d24d3e7a54e3b27a22472f425ee6ff27031c
Kazuki Matsumaru <marukaz.jh@gmail.com>
2019-08-23 15:37:33 +0900

    Install xsel

--- setup.sh
+++ setup.sh
@@ -2,6 +2,13 @@
 
 DOTPATH=~/repos/dotfiles
 
+# Install xsel for tmux copy mode
+if type "apt" > /dev/null 2>&1; then
+    if !(type "xsel" > /dev/null 2>&1); then
+	sudo apt install xsel
+    fi
+fi
+
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
