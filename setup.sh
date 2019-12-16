81368fd2c8b9269d52fd4526e8c99e9adb8ee597
Kazuki Matsumaru <marukaz.jh@gmail.com>
2019-12-16 21:13:18 +0900

    make gitignore_global

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ DOTPATH=~/dotfiles
 # Install xsel for tmux copy mode
 if type "apt" > /dev/null 2>&1; then
     if [ ! `type "xsel"` > /dev/null 2>&1 ]; then
-	sudo apt install xsel
+	    sudo apt install xsel
     fi
 fi
 
@@ -24,3 +24,6 @@ do
 
     ln -sfv "$DOTPATH/$f" "$HOME/$f"
 done
+
+# Activate .gitignore_global
+git config --global core.excludesFile ~/.gitignore_global
