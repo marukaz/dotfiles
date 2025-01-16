9ec26904f28edefcc49ad7f445604e2e70a1f082
Kazuki Matsumaru <Kazuki.Matsumaru@ibm.com>
2025-01-16 17:46:35 +0900

    Update

--- setup.zsh
+++ setup.zsh
@@ -81,7 +81,7 @@ $(brew --prefix)/opt/fzf/install
 
 echo "Setting up prezto ..."
 if ! [ -d "${ZDOTDIR:-$HOME}/.zprezto" ]; then
-  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  git clone --recursive https://github.com/marukaz/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
   setopt EXTENDED_GLOB
   for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
